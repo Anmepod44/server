68dbcbb4e650f9c91d6929653cf3e41ef3fa16c4
DROID <mnyaanga44@gmail.com>
2024-08-24 16:42:13 -0400

    update

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def greet_user():
-    return "Hello, Welcome to the Flask Application!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
